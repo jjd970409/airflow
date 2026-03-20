76fc228e958d5e20f265fc41121c370560c7ebea
jwjung <jwjung@hutrion.co.kr>
2026-03-20 10:23:16 +0900

    kwargs 작성

---
+++ dags/dags_python_with_op_kwargs.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.providers.standard.operators.python import PythonOperator
+from common.common_func import regist2
+
+with DAG(
+    dag_id="dags_python_with_op_kwargs",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2026, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    
+    regist2_t1 = PythonOperator(
+        task_id="regist2_t1",
+        python_callable=regist2,
+        op_args=["홍길동", "남자", "kr", "seoul"],
+        op_kwargs={"email": "hong@example.com", "phone": "010-1234-5678"}
+    )
+
+    regist2_t1
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -6,3 +6,14 @@ def regist(name, sex, *args):
     print(f"이름: {name}")
     print(f"성별: {sex}")
     print(f"추가 정보: {args}") 
+
+def regist2(name, sex, *args, **kwargs):
+    print(f"이름: {name}")
+    print(f"성별: {sex}")
+    print(f"추가 정보: {args}")
+    email = kwargs.get['email'] or None
+    phone = kwargs.get['phone'] or None
+    if email:
+        print(f"이메일: {email}")
+    if phone:
+        print(f"전화번호: {phone}")    

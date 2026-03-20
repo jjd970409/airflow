60922fe76e1f8cd7b90dd5c1fc523ecef61ca25f
jwjung <jwjung@hutrion.co.kr>
2026-03-20 10:30:58 +0900

    ad

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -11,8 +11,8 @@ def regist2(name, sex, *args, **kwargs):
     print(f"이름: {name}")
     print(f"성별: {sex}")
     print(f"추가 정보: {args}")
-    email = kwargs.get['email'] or None
-    phone = kwargs.get['phone'] or None
+    email = kwargs.get('email') or None
+    phone = kwargs.get('phone') or None
     if email:
         print(f"이메일: {email}")
     if phone:
